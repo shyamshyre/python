dd0454d18f6475130b212fbaaed01dcd899c1b5d
shyam <shyam.shyre@gmail.com>
2021-01-27 00:09:10 +0530

    Alphabet patterns

---
+++ 4CONTROLSTATEMENTS/patterns.py
@@ -0,0 +1,66 @@
+#Program 1
+# Input:5
+# Output: * * * * *
+# no = int(input("Enter the input number"))
+# for i in range(no):
+#     print("*",end=' ')
+
+# Input:5
+# Output: * * * 
+        # * * * 
+        # * * * 
+
+
+#Program 2
+# no1 = int(input("Enter the number"))
+# for i in range(no1):
+#     for j in range(no1):
+#            print("*",end=' ')
+#     print()
+# Output :: 
+# * * * 
+# * * * 
+# * * * 
+
+#Program 3
+# no1=int(input("Enter the number"))
+# for i in range(no1):
+#     print("*"*no1)
+# Output::***
+#         ***
+#         ***
+
+#Program 4
+# Expected Result: 
+# 111
+# 222
+# 333
+
+# no1=int(input("Enter the number"))
+# idx=1
+# for i in range(no1):
+#     print(str(idx)*no1)
+#     idx+=1
+# Output::
+# Enter the number4
+# 1111
+# 2222
+# 3333
+# 4444    
+
+#Program 5
+# Enter the input :4
+# AAAA
+# BBBB
+# CCCC
+# DDDD
+# idx=0
+# no = int(input("Enter the number"))
+# for i in range(no):
+#     print(chr(65+idx)*no)
+#     idx+=1
+
+# Enter the number3
+# AAA
+# BBB
+# CCC
