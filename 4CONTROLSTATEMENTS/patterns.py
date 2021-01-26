7becdf0c66bc230a4b2219f7b9dc7d68116b3c99
shyam <shyam.shyre@gmail.com>
2021-01-27 00:45:40 +0530

    Inverted Triangle Completed

--- 4CONTROLSTATEMENTS/patterns.py
+++ 4CONTROLSTATEMENTS/patterns.py
@@ -64,3 +64,50 @@
 # AAA
 # BBB
 # CCC
+
+
+# Program 6
+# *
+# * *
+# * * *
+# idx=1
+# no= int(input("Enter the input number"))
+# for i in range(no):
+#     print('*'*idx)
+#     idx+=1
+
+# Output::
+# Enter the input number3
+# *
+# **
+# ***
+
+
+# Program 7
+# Wap to print the table of your choice
+# no = int(input("Enter the table of your choice"))
+# idx=1
+# value=0
+# for i in range(11):   
+#     value = no*idx
+#     print(str(no)+"*"+str(idx)+"="+str(value))
+#     idx+=1
+
+
+# Program 8
+# * * *
+# * *
+# * 
+
+# idx=1
+# no= int(input("Enter the input number"))
+# for i in range(no):
+#     print('*'*no) 
+#     no-=1    
+
+# Output:
+# ***
+# **
+# *
+
+
