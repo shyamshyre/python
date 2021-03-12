af68949e5cb256734a91652f53678056de10971a
shyamshyre <shyam.shyre@gmail.com>
2021-03-12 15:49:01 +0530

    commit

---
+++ Practise/list.py
@@ -0,0 +1,50 @@
+# Revision
+# l=[]
+# l1=[10,20,30]
+# Insertion of elements inside the list.
+# print("#"*100)
+# print("Insertion of elements inside the list")
+# print("#"*100)
+# l.append(10)
+# l.insert(0,10)
+# l.extend(l1)
+# print(l)
+# print("#"*100)
+# print("Removing of elements form the list")
+# print("#"*100)
+# print(l.remove(10))
+# print(l)
+# print(l.pop())
+# print(l)
+# print(l.clear())
+# print(l)
+
+# print("#"*100)
+# print("Searching the elements inside the list")
+# print("#"*100)
+# l1=[10,20,30]
+# print(l1.index(10))
+# # Value error if element not in the list
+
+# print()
+
+
+# Expression of List
+# l1=["Mary","kathie","belssy"]
+# l2=["Jim","Tom","Peter","Mary"]
+# l1=[i for i in l2 if i not in l1]
+# # print(l1[0:100])
+# # print(l1[0:100: -1])
+# s="Welecome"
+# print(s[100:0: -1])
+
+# set 
+add()
+update()
+
+
+
+
+
+
+
